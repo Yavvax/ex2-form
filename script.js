46337392d9bda38afc7a31453a265f21fe16d0dc
Xavier Lagacé <6151325@cegepoutaouais.qc.ca>
2024-01-23 11:05:15 -0500

    leggo

--- script.js
+++ script.js
@@ -2,56 +2,36 @@
 //https://www.youtube.com/watch?v=g0VXhAYkDm4
 
 function Validation() {
-    let nomFac = document.getElementById("nom").value
-    let erreurNom = document.getElementById("erreur-nom-fac")
-    if (nomFac == null) {
-        erreurNom.textContent = "Le nom doit être rempli"
-        return false
-    }
-    else {
-        erreurNom.textContent = ""
-        return false
-    }
-
-    let adresseFac = document.getElementById("adresse").value
-    let erreurAdd = document.getElementById("erreur-add-fac")
-    if (adresseFac == null) {
-        erreurAdd.textContent = "Le nom doit être rempli"
-        return false
-    }
-    else {
-        erreurAdd.textContent = ""
-        return false
-    }
 
     let livCheck = document.getElementById("checkliv").checked
-    if (livCheck == true) {
+    let nomLiv = document.getElementById("nom2").value
+    let adresseLiv = document.getElementById("adresse2").value
+    if (livCheck) {
+        document.getElementById("nom2").value = document.getElementById("nom").value
+        document.getElementById("adresse2").value = document.getElementById("adresse").value
 
     }
     else {
 
-        let nomLiv = document.getElementById("nom2").value
-        let erreurNom2 = document.getElementById("erreur-nom-liv")
-
-        if (nomLiv == null) {
-            erreurNom2.textContent = "Le nom doit être rempli"
+        if (nomLiv == "") {
+            document.getElementById("erreur-nom-liv").classList.remove("hidden")
             return false
         }
         else {
-            erreurNom2.textContent = ""
-            return false
+            document.getElementById("erreur-nom-liv").classList.add("hidden")
+
         }
 
-        let adresseLiv = document.getElementById("adresse2").value
+
         let erreurAdd2 = document.getElementById("erreur-add-liv")
 
-        if (adresseLiv == null) {
-            erreurAdd2.textContent = "Le nom doit être rempli"
+        if (adresseLiv == "") {
+            document.getElementById("erreur-add-liv").classList.remove("hidden")
             return false
         }
         else {
-            erreurAdd2.textContent = ""
-            return false
+            document.getElementById("erreur-add-liv").classList.add("hidden")
+
         }
     }
 
